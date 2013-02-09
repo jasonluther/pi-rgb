6c55036402eae5e250c3d5cb77cd4425e21eb8e4
Jason Luther <jason@ixid.net>
2013-02-08 23:51:23 -0500

    having trouble writing out the 1's and 0's

--- rgb.c
+++ rgb.c
@@ -6,24 +6,25 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-unsigned char RGB_VALUE[3] = { 128, 128, 128 };
+unsigned char RGB_VALUE[3] = { 0, 0, 0 };
 int RGB_PIN[3] = { 4, 5, 6 };
 int time_to_update = 0;
 
 void update_rgb(void) {
   int i, fd, bytes_read;
-  char color[3]; /* 3 bytes */
+  unsigned char color[3]; /* 3 bytes */
   if ((fd = open("/var/rgbcolor", O_RDONLY)) < 0) {
-    fprintf(stderr, "Unable to open /var/rgbcolor: %s\n", strerror (errno));
+    fprintf(stderr, "Unable to open /var/rgbcolor: %s\n", strerror(errno));
   }
   if ((bytes_read = read(fd, color, 3)) < 3) {
-    fprintf(stderr, "Didn't read all 3 bytes: %s\n", strerror (errno));
+    fprintf(stderr, "Read %d bytes: %s\n", bytes_read, strerror(errno));
   }
   for (i = 0; i < 3; i++) {
     RGB_VALUE[i] = color[i];
+    fprintf(stdout, "%d: %x\n", i, RGB_VALUE[i]);
   }
   if (close(fd) < 0) {
-    fprintf(stderr, "Unable to close /var/rgbcolor: %s\n", strerror (errno));
+    fprintf(stderr, "Unable to close /var/rgbcolor: %s\n", strerror(errno));
   }
 }
 
@@ -31,13 +32,13 @@ int main(int argc, char *argv[]) {
   int i;
 
   if (wiringPiSetup () < 0) {
-    fprintf (stderr, "wiringPiSetupGpio failed: %s\n", strerror (errno));
+    fprintf (stderr, "wiringPiSetupGpio failed: %s\n", strerror(errno));
     return 1;
   }
 
   for (i = 0; i < 3; i++) {
     if (softPwmCreate(RGB_PIN[i], 0, 255) < 0) {
-      fprintf (stderr, "softPwmCreate failed: %s\n", strerror (errno));
+      fprintf (stderr, "softPwmCreate failed: %s\n", strerror(errno));
       return 1;
     }
   }
@@ -46,6 +47,7 @@ int main(int argc, char *argv[]) {
     update_rgb();
     
     for (i = 0; i < 3; i++) {
+      fprintf(stdout, "softPwmWrite %d: %d\n", RGB_PIN[i], RGB_VALUE[i]);
       softPwmWrite(RGB_PIN[i], RGB_VALUE[i]) ;
     }
     delay(1000);
---
+++ write_color.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl -w
+use strict;
+
+my $color_string = shift;
+chomp $color_string;
+print "Input: $color_string\n";
+my ($red, $green, $blue);
+if ($color_string =~ /#?(..)(..)(..)/) {
+  ($red, $green, $blue) = map { hex($_) } ($1, $2, $3);
+} else {
+  die "You didn't provide a format that satisfied me.";
+}
+
+open(OUT, ">/var/rgbcolor") or die "Unable to open $!";
+binmode(OUT);
+print "Red: $red\n";
+print "Green: $green\n";
+print "Blue: $blue\n";
+print OUT $red;
+print OUT $green;
+print OUT $blue;
+close(OUT);
+
+print `cat /var/rgbcolor`;
