d4bb4f30e878363377caed8609da638304725d97
Jason Luther <jason@ixid.net>
2013-02-21 20:21:58 -0500

    set the mode

--- write_rgbcmd.c
+++ write_rgbcmd.c
@@ -10,9 +10,13 @@
 int main(int argc, char *argv[]) {
   rgbcmd_t *cmd;
   cmd = open_rgbcmd(1);
-  cmd->mode = RGB_MODE_ON;
   cmd->red = atoi(argv[1]);
   cmd->green = atoi(argv[2]);
   cmd->blue = atoi(argv[3]);
+  if (argc == 5) {
+    cmd->mode = atoi(argv[4]);
+  } else {
+    cmd->mode = RGB_MODE_ON;
+  }
   return 0;
 }
