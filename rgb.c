de89afa2271a9d8dd8d910b142fcd687bad63952
Jason Luther <jason@ixid.net>
2013-02-22 19:03:00 -0500

    Oh, rainBOWS

--- rgb.c
+++ rgb.c
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <math.h>
 #include "rgbcmd.h"
 
 const int PIN_RED = 4;
@@ -13,9 +14,11 @@ const int PIN_GREEN = 5;
 const int PIN_BLUE = 6;
 rgbcmd_t prev_cmd;
 
-unsigned long NEXT_TIME_TO_BLINK;
+const int DELAY = 10;
+
+unsigned long NEXT_TIME_TO_BLINK = 0;
 int BLINK_STATE = 0;
-#define BLINK_MS 1000;
+const unsigned long BLINK_MS = 1000;
 int time_to_blink(void) {
   unsigned long now = millis();
   if (now >= NEXT_TIME_TO_BLINK) {
@@ -25,10 +28,58 @@ int time_to_blink(void) {
   return BLINK_STATE;
 }
 
+int HUE = 0;
+/* Adapted from http://www.cs.rit.edu/~ncs/color/t_convert.html */
+/* but hue = 0-359, assume S & V are 1. RGB are 0-255. */
+void update_rgbcmd_from_hue(int hue, rgbcmd_t *rainbow_cmd) {
+  int sector;
+  float fraction, q, t;
+  hue %= 360;
+  sector = hue / 60;
+  fraction = (hue % 60) / 60.0;
+  t = 255 * fraction;
+  q = 255 - t;
+  switch(sector) {
+    case 0:
+      rainbow_cmd->red = 255;
+      rainbow_cmd->green = t;
+      rainbow_cmd->blue = 0;
+      break;
+    case 1:
+      rainbow_cmd->red = q;
+      rainbow_cmd->green = 255;
+      rainbow_cmd->blue = 0;
+      break;
+    case 2:
+      rainbow_cmd->red = 0;
+      rainbow_cmd->green = 255;
+      rainbow_cmd->blue = t;
+      break;
+    case 3:
+      rainbow_cmd->red = 0;
+      rainbow_cmd->green = q;
+      rainbow_cmd->blue = 255;
+      break;
+    case 4:
+      rainbow_cmd->red = t;
+      rainbow_cmd->green = 0;
+      rainbow_cmd->blue = 255;
+      break;
+    case 5:
+      rainbow_cmd->red = 255;
+      rainbow_cmd->green = 0;
+      rainbow_cmd->blue = q;
+      break;
+    default: 
+      fprintf(stderr, "Did you pass in 360 or higher: %d?\n", hue);
+      exit(-1); 
+      break;
+  }
+}
+
 int main(int argc, char *argv[]) {
   rgbcmd_t *user_cmd, *current_cmd;
   rgbcmd_t off_cmd, rainbow_cmd;
-  NEXT_TIME_TO_BLINK = 0;
 
   /* wiringPiSetup() uses pins 0-17, wiringPiSetupGpio() uses BCM pins! */
   if (wiringPiSetup() < 0) {
@@ -60,9 +111,8 @@ int main(int argc, char *argv[]) {
         break;
       case RGB_MODE_RAINBOW:
         current_cmd = &rainbow_cmd;
-        current_cmd->red++;
-        current_cmd->green++;
-        current_cmd->blue++;
+        HUE++; if (HUE > 360) { HUE = 0; }
+        update_rgbcmd_from_hue(HUE, &rainbow_cmd);
         break;
       case RGB_MODE_BLINK:
         if (time_to_blink()) {
@@ -75,10 +125,11 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Invalid mode %d\n", user_cmd->mode);
         break;
     }
+    //fprintf(stderr, "RGB %d %d %d\n", current_cmd->red, current_cmd->green, current_cmd->blue);
     softPwmWrite(PIN_RED, current_cmd->red) ;
     softPwmWrite(PIN_GREEN, current_cmd->green) ;
     softPwmWrite(PIN_BLUE, current_cmd->blue) ;
-    delay(500);
+    delay(DELAY);
   }
 
 }
